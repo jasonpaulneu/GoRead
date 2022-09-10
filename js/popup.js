4fa871828e9eee17b46840b88c3859e038f4845a
Jason Paul Darivemula <darivemula.j@northeastern.edu>
2022-09-10 18:08:11 -0400

    Add EventListener selectLanguageOnChange

--- js/popup.js
+++ js/popup.js
@@ -1,30 +1,57 @@
-// Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
+/**
+ * Things to do
+ * 1. Set language
+ * 2. Set number of words
+ * 3. Set difficulty
+ */
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
+//get selected language and set it as a value for the selected input
+chrome.storage.sync.get(['selectedLanguage'], (result) => {
+  let selectedLanguageElement = document.getElementById("language-select");
+  console.log("result",result)
+  selectedLanguageElement.value=result.selectedLanguage;
 });
 
-/**
- * SAMPLE CODE FOR UNDERSTANDING HOW TO MANIPULATE DOM 
- *  
-*/ 
+//event listener function for selectElement
+const selectLanguageOnChange = () =>{
+  let selectedLanguage = document.getElementById("language-select").value;
+  chrome.storage.sync.set({selectedLanguage:selectedLanguage});
+}
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+//add event listener to select element
+let selectedLanguageElement = document.getElementById("language-select");
+selectedLanguageElement.addEventListener('change',selectLanguageOnChange);
 
-    console.log(chrome);
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      func: setPageBackgroundColor,
-    });
-  });
+
+
+
+// // Initialize button with user's preferred color
+// let changeColor = document.getElementById("changeColor");
+
+// chrome.storage.sync.get("color", ({ color }) => {
+//   changeColor.style.backgroundColor = color;
+// });
+
+// /**
+//  * SAMPLE CODE FOR UNDERSTANDING HOW TO MANIPULATE DOM 
+//  *  
+// */ 
+
+// // When the button is clicked, inject setPageBackgroundColor into current page
+// changeColor.addEventListener("click", async () => {
+//     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+//     console.log(chrome);
+//     chrome.scripting.executeScript({
+//       target: { tabId: tab.id },
+//       func: setPageBackgroundColor,
+//     });
+//   });
   
-  // The body of this function will be executed as a content script inside the
-  // current page
-  function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-      document.body.style.backgroundColor = color;
-    });
-  }
+//   // The body of this function will be executed as a content script inside the
+//   // current page
+//   function setPageBackgroundColor() {
+//     chrome.storage.sync.get("color", ({ color }) => {
+//       document.body.style.backgroundColor = color;
+//     });
+//   }
