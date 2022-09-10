2e31a4b7f85792f13e765e1c36145f3e9b280a42
parthasarathydNU <parthasarathy.d@northeastern.edu>
2022-09-10 17:27:26 -0400

    Starter code for picking words form the dom

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,9 @@
 // Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
+// let changeColor = document.getElementById("changeColor");
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
+// chrome.storage.sync.get("color", ({ color }) => {
+//   changeColor.style.backgroundColor = color;
+// });
 
 /**
  * SAMPLE CODE FOR UNDERSTANDING HOW TO MANIPULATE DOM 
@@ -11,20 +11,20 @@ chrome.storage.sync.get("color", ({ color }) => {
 */ 
 
 // When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+// changeColor.addEventListener("click", async () => {
+//     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-    console.log(chrome);
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      func: setPageBackgroundColor,
-    });
-  });
+//     console.log(chrome);
+//     chrome.scripting.executeScript({
+//       target: { tabId: tab.id },
+//       func: setPageBackgroundColor,
+//     });
+//   });
   
   // The body of this function will be executed as a content script inside the
   // current page
-  function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-      document.body.style.backgroundColor = color;
-    });
-  }
+  // function setPageBackgroundColor() {
+  //   chrome.storage.sync.get("color", ({ color }) => {
+  //     document.body.style.backgroundColor = color;
+  //   });
+  // }
