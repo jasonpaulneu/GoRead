4f655de92c3b7ae64a77a5d9203742df969b6332
parthasarathydNU <parthasarathy.d@northeastern.edu>
2022-09-10 23:44:07 -0400

    Code for replacing with translated text

--- js/vamos.js
+++ js/vamos.js
@@ -22,7 +22,7 @@ translateButton.addEventListener("click", async () => {
 // current page
 async function translateFunction() {
 
-  console.log("I am going to start translating the content on this page!");
+  // console.log("I am going to start translating the content on this page!");
 
   // picking the p tags on the page
 
@@ -53,12 +53,117 @@ async function translateFunction() {
 
   // once we have the words we send this to the function that does the API call and get's us the translated data
 
-  let translatedWords = await getTranslatedWords(randomWords);
+  // let translatedWords = await getTranslatedWords(randomWords); // actual function call
+  let translatedWords = tempTranslate(randomWords); // actual function call
 
 
-  console.log("Translated words");
-  console.log(translatedWords);
 
+
+  //===============================
+
+  /**
+   * Now that we have translated the words, let's go ahead with replacing the words in the dom with the translated words
+   */
+  replaceDom(domElems, randomWords, translatedWords, 3);
+
+
+  /**
+   * This function access the dom elements and swaps the original words with the translated words
+   * This also adds the tag to surround those elements so that we can add the required styling and the tooltip
+   * @param {HtmlElement} domElements - the dom elements that have to be modified
+   * @param {String} wordsToReplace - words that have to be replaed
+   * @param {String} replacementWords - words that are translated
+   * @param {Number} frequency - number of words to replace per paragrph tag
+   */
+  function replaceDom(domElements, wordsToReplace, replacementWords, frequency){
+    
+    /**
+     * To replace the dom elements, we know that for every dom element we have 'frequency' number of replacements to make
+     * and the words are present in the same order
+     */
+    // console.log("Ulle poichu");
+    let count = 0;
+    // we run a for loop for each dom element
+    for(let i = 0; i < domElements.length; i++){
+      let domElem = domElements.item(i);
+
+      // slice function, start is inclusive, end is exclusive
+      let targetWords = wordsToReplace.slice(count, count+frequency);
+      
+
+      let swapWords = replacementWords.slice(count, count+frequency);
+      
+
+      // for each target word identify the position of that in the pTags
+
+      // now we have this dom element, that is most probably a paragraph tag
+      
+      // we run a for loop and replace the 3 random words with the new word that we selected
+      
+      for(let i = 0 ; i < targetWords.length; i++){
+        let targetWord = targetWords[i];
+        let replacement = swapWords[i];
+
+
+
+
+        // now we filter through the p tag and get that text tag and see if this p tag has any text 
+        // that can be highlighted
+
+        for(let j = 0; j < domElem.childElementCount; j++){
+
+          let el = domElem.childNodes[j];
+
+
+          if(el.nodeType === 3){
+            
+            // check if this element contains that word
+            let value = el.nodeValue;
+            
+            if(value.includes(targetWord)){
+
+              // yes it contains, we replace that with a new span element
+              let newSpan = document.createElement("span");
+
+              // console.log(value);
+              console.log(targetWord);
+              console.log(replacement);
+              value = value.replace(targetWord, `<span class="hoverElement">${replacement}</span>`);
+              // console.log(value);
+              newSpan.innerHTML = value;
+
+              el.replaceWith(newSpan);
+            }
+          }
+        }
+
+
+      }
+
+      count += 3;
+    }
+
+  }
+
+  /**
+   * This is a temp function that returns hard coded transated words
+   * using this to proeed with other functionality
+   * 
+   * this takes in an array of words and returns the same array with the characters
+   * "-tr" attached after each word
+   * @param {String} stringArray 
+   * @returns 
+   */
+  function tempTranslate(stringArray){
+
+    return  stringArray.map(x => x=x+"-tr");
+    
+  }
+  
+  /**
+   * This function takes in an array of string and triggers the API call to get the translated data
+   * @param {String} arrayOfwords 
+   */
   async function getTranslatedWords(arrayOfwords) {
 
     textBody = []
@@ -100,11 +205,12 @@ async function translateFunction() {
   }
 
   /**
- * This function takes in a string and returns an array of string
+ * This function takes in a string and returns an array of words that we want to translate
  * @param {String} text 
  */
   function getRandomWords(text) {
 
+    // console.log(text);
     // we set the number of words that we wanna translate in a sentence
     let x = 3;
 
@@ -112,13 +218,17 @@ async function translateFunction() {
     let arr = [];
 
     for (let i = 0; i < x; i++) {
-      arr.push(text.split(" ")[Math.floor(Math.random() * text.split(" ").length)]);
+      let splitArray= text.split(" ");
+      let len = splitArray.length;
+      // console.log(splitArray);
+      // console.log(splitArray[Math.floor(Math.random() * len)]);
+      arr.push(splitArray[Math.floor(Math.random() * len)]);
     }
 
+    console.log(arr);
+
+
     return arr;
-
-
-
   }
 }
 
